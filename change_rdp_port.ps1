b6497d0aa20097467eb9cb5467f7b07a80ed9331
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2024-02-01 13:52:55 -0500

    Update change_rdp_port.ps1

--- change_rdp_port.ps1
+++ change_rdp_port.ps1
@@ -2,5 +2,5 @@ param([int]$portvalue = 3390)
 
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber" -Value $portvalue 
 
-New-NetFirewallRule -DisplayName 'RDPPORTLatest-TCP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $portvalue 
-New-NetFirewallRule -DisplayName 'RDPPORTLatest-UDP-In' -Profile 'Public'
+New-NetFirewallRule -DisplayName 'RDPPORTLatest-TCP-In' -Profile 'All' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $portvalue 
+New-NetFirewallRule -DisplayName 'RDPPORTLatest-UDP-In' -Profile 'All'
