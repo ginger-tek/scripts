5d3f9141700f1250af08d699f648df9c948b6f5c
ginger-tek <jeremy.paul.moormann@gmail.com>
2024-02-02 14:27:12 -0500

    casing, safety checks

--- vbox_create_vm.ps1
+++ vbox_create_vm.ps1
@@ -9,49 +9,54 @@ param(
   [switch]$3DVideoAcceleration,
   [switch]$Unattended,
   [string]$Username,
-  [string]$Password,
+  [string]$Pass,
   [string]$Locale = 'en_US',
   [string]$Timezone = 'EST',
   [string]$StartMode,
   [string]$BasePath
 )
 
-if (!(VBoxManage)) {
+if ($env:Path -notmatch 'VirtualBox') {
   Write-Warning 'VirtualBox CLI not found'
   break
 }
 
-$diskSizeGB *= 1024
+if (!(Test-Path $ISOPath)) {
+  Write-Warning 'ISO not found'
+  break
+}
+
+$DiskSizeGB *= 1024
 
 # Create VM
-if ($basePath) {
-  VBoxManage createvm --name $name --ostype $osType --register --basefolder $basePath
+if ($BasePath) {
+  VBoxManage createvm --name $Name --ostype $OSType --register --basefolder $BasePath
 }
 else {
-  VBoxManage createvm --name $name --ostype $osType --register
-  $basePath = '{0}\VirtualBox VMs\{1}' -f $env:USERPROFILE, $name
+  VBoxManage createvm --name $Name --ostype $OSType --register
+  $BasePath = '{0}\VirtualBox VMs\{1}' -f $env:USERPROFILE, $Name
 }
-VBoxManage modifyvm $Name --ioapic on --memory $ram --vram $vram --cpus $cpus --accelerate-2d-video on --graphicscontroller vmsvga --vrde on --vrdemulticon on --vrdeport 10001
+VBoxManage modifyvm $Name --ioapic on --memory $RAM --vram $VRAM --cpus $CPUs --accelerate-2d-video on --graphicscontroller vmsvga --vrde on --vrdemulticon on --vrdeport 10001
 if ($3DVideoAcceleration.IsPresent) {
   VBoxManage modifyvm $Name --accelerate-3d on
 }
-$disk = '{0}\{1}_disk.vdi' -f $basePath, $name
-VBoxManage createhd --filename $disk --size $diskSizeGB --format VDI
-VBoxManage storagectl $name --name 'SATA Controller' --add SATA --controller IntelAhci
-VBoxManage storageattach $name --storagectl 'SATA Controller' --port 0 --device 0 --type hdd --medium $disk
-VBoxManage storageattach $name --storagectl 'SATA Controller' --port 1 --device 0 --type dvddrive --medium $isoPath
-VBoxManage modifyvm $name --boot1 dvd --boot2 disk --boot3 none --boot4 none
-VBoxManage showvminfo $name
+$disk = '{0}\{1}_disk.vdi' -f $BasePath, $Name
+VBoxManage createhd --filename $disk --size $DiskSizeGB --format VDI
+VBoxManage storagectl $Name --name 'SATA Controller' --add SATA --controller IntelAhci
+VBoxManage storageattach $Name --storagectl 'SATA Controller' --port 0 --device 0 --type hdd --medium $disk
+VBoxManage storageattach $Name --storagectl 'SATA Controller' --port 1 --device 0 --type dvddrive --medium $ISOPath
+VBoxManage modifyvm $Name --boot1 dvd --boot2 disk --boot3 none --boot4 none
+VBoxManage showvminfo $Name
 if ($Unattended.IsPresent) {
-  if (!$Username -and !$Password) {
+  if (!$Username -and !$Pass) {
     Write-Warning 'Missing username and password for account creation'
     break
   }
-  VBoxManage unattended install $name --iso $isoPath --user $username.ToLower() --password $password --full-user-name $username --locale $locale --time-zone $timezone
+  VBoxManage unattended install $Name --iso $ISOPath --user $Username.ToLower() --password $Pass --full-user-name $Username --locale $Locale --time-zone $Timezone
   if ($StartMode) {
-    VBoxManage startvm $name --type $startMode
+    VBoxManage startvm $Name --type $StartMode
   }
   else {
-    VBoxManage startvm $name --type gui
+    VBoxManage startvm $Name --type gui
   }
 }
