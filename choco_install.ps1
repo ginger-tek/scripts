1f0f82bcffad4404228463203a8a5690be49013e
ginger-tek <jeremy.paul.moormann@gmail.com>
2024-02-02 14:10:03 -0500

    set exec to unrestricted, add nvm install + python3

--- choco_install.ps1
+++ choco_install.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Unrestricted
 choco install pwsh
 choco install git.install
 choco install vscode.install
@@ -8,3 +9,6 @@ choco install python3
 choco install 7zip.install
 choco install notepadplusplus
 choco install googlechrome
+choco install python3
+nvm install lts
+nvm use lts
