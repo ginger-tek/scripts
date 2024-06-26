4504342fe4b98bc85f39ff06c36fec3144207c3c
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2024-06-25 20:18:03 -0400

    Update choco_install.ps1

--- choco_install.ps1
+++ choco_install.ps1
@@ -4,11 +4,11 @@ choco install pwsh -y
 choco install git.install -y
 choco install vscode.install -y
 choco install vscode-powershell -y
-choco install nvm.install -y
 choco install php -y
 choco install python3 -y
 choco install 7zip.install -y
 choco install notepadplusplus -y
 choco install python3 -y
+choco install nvm.install -y
 nvm install lts
 nvm use lts
