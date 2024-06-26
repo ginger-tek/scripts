fa3b2d2f40ec2c5e0ac7b53457f95448dd3312f1
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2024-06-25 20:17:41 -0400

    Update choco_install.ps1

--- choco_install.ps1
+++ choco_install.ps1
@@ -1,4 +1,5 @@
 Set-ExecutionPolicy Unrestricted
+choco install googlechrome -y
 choco install pwsh -y
 choco install git.install -y
 choco install vscode.install -y
@@ -8,7 +9,6 @@ choco install php -y
 choco install python3 -y
 choco install 7zip.install -y
 choco install notepadplusplus -y
-choco install googlechrome -y
 choco install python3 -y
 nvm install lts
 nvm use lts
