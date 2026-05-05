434b23f23bf54ab2856ab2c5e001587eb2138688
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2026-05-05 09:59:43 -0400

    add public script files

---
+++ expassnj-export.ps1
@@ -0,0 +1,88 @@
+﻿param(
+    [Parameter(Mandatory=$true)][string]$username,
+    [Parameter(Mandatory=$true)][string]$pass,
+    [string]$outputDir = "$env:USERPROFILE\Downloads"
+)
+
+$ie = New-Object -com InternetExplorer.Application
+$ie.visible=$true
+$ie.silent = $true
+$ie.navigate("https://www.ezpassnj.com/vector/account/home/accountLogin.do") 
+sleep(1)
+$ie.Document.IHTMLDocument3_getElementById("tt_username1").value= "$username" 
+$ie.Document.IHTMLDocument3_getElementById("tt_loginPassword1").value = "$pass" 
+$ie.Document.IHTMLDocument3_getElementById("jcaptcha_response1").value = Read-Host "Captcha" 
+$form = $ie.Document.IHTMLDocument3_getElementsByTagName("form") | where-object {$_.ClassName -eq "form-signincus"}
+$form.submit();
+sleep(2)
+$ie.navigate("https://www.ezpassnj.com/vector/account/transactions/batatransactionSearch.do")
+sleep(2)
+
+$sm = $smi = $em = $emi = 0
+$year = (Get-Date).AddYears(-1).Year
+
+for($i=0;$i -lt 12;$i++) {
+    $sm++
+    if(($sm.ToString()).length -eq 1) {
+        $smi = "0$sm"
+    } elseif(($em.ToString()).length -eq 2) {
+        $smi = "$sm"
+    }
+    $sdate = "$smi/01/$year"
+    $sdate
+
+    $startdateinput = $ie.Document.IHTMLDocument3_getElementById("startDateAll")
+    $startdateinput.value = $sdate
+
+    ############################
+
+    $em = $sm + 1
+    if(($em.ToString()).length -eq 1) {
+        $emi = "0$em"
+    } elseif(($em.ToString()).length -eq 2) {
+        if($em -eq 13) {
+            $em = "01"
+            $year = (Get-Date).Year
+        }
+        $emi = "$em"
+    }
+    $edate = "$emi/01/$year"
+    $edate
+
+    $enddateinput = $ie.Document.IHTMLDocument3_getElementById("endDateAll")
+    $enddateinput.value = $edate
+
+    ############################
+
+    $view = $ie.Document.IHTMLDocument3_getElementsByTagName("button") | where-object {$_.name -eq 'btnView' -and $_.type -eq 'submit'}
+    $view.click();
+    sleep(2)
+    $ie.navigate("https://www.ezpassnj.com/vector/account/transactions/batatransactionSearch.do?printType=xls&exclGen=true")
+    sleep(2)
+
+    ############################
+
+    $obj = new-object -com WScript.Shell
+    $obj.AppActivate('Internet Explorer')
+    $obj.SendKeys('s')
+    sleep(2)
+}
+
+$ie.navigate("https://www.ezpassnj.com/vector/account/home/accountLogout.do")
+sleep(2)
+$ie.Quit()
+
+$year = (Get-Date).AddYears(-1).Year
+
+$CSVFolder = "$outputDir\Transaction*.csv"
+$OutputFile = "$outputDir\EZ-Pass_$year.csv"
+
+$CSV= @()
+
+Get-ChildItem -Path $CSVFolder -Filter *.csv | ForEach-Object { 
+    $CSV += @(Import-Csv -Path $_)
+}
+
+$CSV | Export-Csv -Path $OutputFile -NoTypeInformation -Force
+
+#Remove-Item -Path "$outputDir\Transaction*.csv"
---
+++ snow_custom_scripts.js
@@ -0,0 +1,97 @@
+window.onload = () => {
+	setTimeout(() => {
+		console.log('Running scripts');
+		addCalBtn();
+		ctrlSave();
+		preloadChangeCom();
+		noFocus();
+	}, 1);
+};
+
+function addCalBtn() {
+	const changeNum = document.getElementById('change_request.number');
+	if (!changeNum) return;
+	console.info('[ChangeNum]', changeNum.value);
+	const btn = document.createElement('button');
+	btn.type = 'button';
+	btn.width = 'auto';
+	btn.classList.add('btn', 'btn-default');
+	btn.innerText = 'Add to Calendar';
+	btn.addEventListener('click', () => {
+		const calLink = createOutlookCalLink({
+			sysId: document.getElementById('sys_uniqueValue').value,
+			number: changeNum.value,
+			description: document.getElementById('change_request.description').value,
+			summary: document.getElementById('change_request.short_description')
+				.value,
+			startDate: document.getElementById('change_request.start_date').value,
+			endDate: document.getElementById('change_request.end_date').value,
+		});
+		if (!calLink) {
+			console.error('Failed to create calendar link');
+			return;
+		}
+		window.open(calLink);
+	});
+	const wrapSpan = document.createElement('span');
+	wrapSpan.appendChild(btn);
+	const btnWrap = document.createElement('div');
+	btnWrap.classList.add(
+		'col-xs-2',
+		'col-sm-3',
+		'col-lg-2',
+		'form-field-addons'
+	);
+	btnWrap.appendChild(wrapSpan);
+	changeNum.parentElement.parentElement.appendChild(btnWrap);
+}
+
+function createOutlookCalLink(data) {
+	if (!data.number || !data.summary || !data.startDate || !data.endDate)
+		return null;
+	const url = `${location.origin}/nav_to.do?uri=change_request.do?sys_id=${data.sysId}`;
+	const params = new URLSearchParams();
+	params.set('allday', 'false');
+	params.set(
+		'body',
+		`Link: <a href="${url}">${data.number}</a><br>${data.description.replace('\r\n', '<br>')}`
+	);
+	params.set('startdt', new Date(data.startDate).toISOString());
+	params.set('enddt', new Date(data.endDate).toISOString());
+	params.set('path', '/calendar/action/compose');
+	params.set('rru', 'addevent');
+	params.set('subject', `${data.number}: ${data.summary}`);
+	return `https://outlook.office365.com/calendar/0/action/compose?${params.toString()}`;
+}
+
+function ctrlSave() {
+	window.addEventListener('keydown', (e) => {
+		if (e.ctrlKey && e.key == 's') {
+			e.preventDefault();
+			e.target?.blur();
+			setTimeout(() => {
+				console.info('Saving record');
+				if (gsftSubmit) gsftSubmit(gel('sysverb_update_and_stay'));
+				else console.warn('gsftSubmit not defined, iframe not in focus');
+			});
+		}
+	});
+}
+
+function preloadChangeCom() {
+	const textbox = document.getElementById(
+		'change_request.u_communication_plan'
+	);
+	const com = localStorage.getItem('change_com');
+	if (textbox && !textbox.value && com) {
+		textbox.focus();
+		textbox.value = com;
+		textbox.dispatchEvent(new Event('input', { bubbles: true }));
+		textbox.blur();
+	}
+}
+
+function noFocus() {
+	(document.getElementById('incident.short_description') ||
+		document.getElementById('change_request.short_description'))?.focus();
+}
